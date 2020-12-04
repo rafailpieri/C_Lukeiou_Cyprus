6ccadcaf68bc0b6007350f9d4275ce47d3b9b719
rafailpieri <75477918+rafailpieri@users.noreply.github.com>
2020-12-04 17:37:23 +0200

    Create 4_24.cpp

---
+++ Ενότητα 4/4_24.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int min3(int a,int b,int c){
+
+    if(a>b&&a>c)
+        return a;
+
+     if(b>a&&b>c)
+        return b;
+
+     if(c>a&&c>b)
+        return c;
+
+
+}
+
+int main(){
+
+int num1,num2,num3,max;
+
+    cout<<"Dose 3 arithmus"<<endl;
+    cin>>num1>>num2>>num3;
+
+    max= min3(num1,num2,num3);
+
+   cout<<"O megaliteros arithmos einai: "<<max;
+
+return 0;
+}
